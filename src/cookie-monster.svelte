7a35867626027259484e527ae5fb95f18a10e6c7
Gianluca Guarini <gianluca.guarini@gmail.com>
2020-01-20 13:10:10 +0100

    [frontend-setup] Remove unecessary stuff

--- src/cookie-monster.svelte
+++ src/cookie-monster.svelte
@@ -2,15 +2,7 @@
     <h3 class="cookie-monster-banner--title">{$_('title', { default: 'Cookie Title' })}</h3>
 
     <p class="cookie-monster-banner--content">
-        {$_('text', { default: 'Cookie Text' })}
-
-        {#if helpText}
-            {#if policyURL}
-                <a href="{policyURL}">{helpText}</a>
-            {:else}
-                {helpText}
-            {/if}
-        {/if}
+        {@html $_('text', { default: 'Cookie Text' })}
     </p>
 
     <div class="cookie-monster-banner--content">
@@ -20,57 +12,20 @@
             </button>
         </div>
     </div>
-
-    <p class="cookie-monster-banner--warning">{$_('messages.warning', { default: 'Warning message' })}</p>
 </div>
 
 <style>
     .cookie-monster-banner {
         display: none;
         width: auto;
-        min-height: 20px;
-        padding: 30px;
-        background: #ffffff;;
         position: fixed;
         bottom: 0;
-        border: 20px;
-        box-shadow: 0 0 16px #333;
         opacity: 0;
     }
 
     .cookie-monster-banner.cookie-monster-banner__active {
         display: block;
         opacity: 1;
-    }
-
-    .cookie-monster-banner--title {
-        font-size: 25px;
-    }
-
-    .cookie-monster-banner--content p, fieldset, a {
-        font-size: 16px;
-    }
-
-    .cookie-monster-banner--content a {
-        text-decoration: underline;
-    }
-
-    .cookie-monster-banner--btn-wrapper .cookie-monster-banner--btn {
-        display: inline-block;
-        color: #000000;
-        font-size: 16px;
-        margin-top: 10px;
-        background: #4caf50;
-        padding: 8px 15px;
-        text-align: center;
-        text-decoration: none;
-        cursor: pointer;
-    }
-
-    .cookie-monster-banner--warning {
-        color: #b94a48;
-        background-color: #f2dede;
-        border-color: #eed3d7;
     }
 </style>
 
@@ -79,12 +34,10 @@
     import { hasAcceptedCookies, createCookie } from './util';
 
     // Props
-    export let policyURL;
     export let cookieId;
     export let cookieAge;
 
     // reactive props
-    let helpText = $_('help', { default: '' });
     let isActive = !hasAcceptedCookies(cookieId);
     let selectedCookies = [];
 
