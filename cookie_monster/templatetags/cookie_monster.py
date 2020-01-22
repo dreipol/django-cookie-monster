808ecba1dfb875668f2337344bc479b7e9583050
Gianluca Guarini <gianluca.guarini@gmail.com>
2020-01-22 15:39:07 +0100

    Remove the group template

--- cookie_monster/templatetags/cookie_monster.py
+++ cookie_monster/templatetags/cookie_monster.py
@@ -16,15 +16,6 @@ def cookie_base(context):
     return context
 
 
-@register.inclusion_tag('cookie_monster/group.html', takes_context=True)
-def cookie_group(context):
-    context['COOKIE_BASE'] = COOKIE_MONSTER_BASE_CONFIG
-    context['COOKIE_CUSTOM_THEME'] = COOKIE_MONSTER_CUSTOM_THEME
-    context['COOKIE_GROUP'] = COOKIE_MONSTER_GROUP_CONFIG
-    context['COOKIE_GROUP_WARNING'] = COOKIE_GROUP_WARNING
-    return context
-
-
 @register.simple_tag(takes_context=True)
 def get_cookie_policy_url(context):
     href_callable = COOKIE_MONSTER_BASE_CONFIG['cookie_policy']['href_callable']
